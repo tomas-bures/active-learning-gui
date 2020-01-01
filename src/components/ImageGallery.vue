7536c5b2b22058fb9df73bc1525c62495ce0be67
Tomáš Bureš <burgi22@seznam.cz>
2020-01-01 18:32:27 +0100

    Setup firebase

---
+++ src/components/ImageGallery.vue
@@ -0,0 +1,23 @@
+<template>
+  <div>
+    <ul>
+      <li>
+        
+      </li>
+    </ul>
+  </div>   
+</template>
+
+<script>
+import VueLazyload from "vue-lazyload";
+
+Vue.use(VueLazyload);
+
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
