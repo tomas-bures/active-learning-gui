2cc9633b2f5c1c68b67d37c0d5ff3821f60f94e1
Tomáš Bureš <burgi22@seznam.cz>
2020-02-06 21:28:20 +0100

    Implement method loadImages for image loading from firebase storage

--- src/components/ImageGallery.vue
+++ src/components/ImageGallery.vue
@@ -4,11 +4,11 @@
     <v-slider v-model="size" min="100" max="1100" thumb-label="always"></v-slider>
     <v-container class="my-5">
       <v-layout row md5 justify-space-around>
-        <v-flex class="my-4" v-for="n in 500" :key="n">
+        <v-flex class="my-4" v-for="image in images" :key="image">
           <v-lazy :options="{
               threshold: 0.5
             }">
-            <Picture :size="size" />
+            <Picture :size="size" :source="image" />
           </v-lazy>
         </v-flex>
       </v-layout>
@@ -16,8 +16,9 @@
   </div>
 </template>
 
-<script lang="ts">
+<script>
 import Picture from "./Picture.vue";
+import { FirebaseStorage } from "@/firebase/storage";
 
 export default {
   name: "image-gallery",
@@ -25,10 +26,33 @@ export default {
     Picture
   },
   data: () => ({
-    images: new Array<String>(),
-    size: 350
+    images: new Array(),
+    references: new Array(),
+    size: 350,
+    folder: "val2017",
+    storageRef: FirebaseStorage.ref()
   }),
-  mounted() {}
+  mounted() {
+    this.loadImages();
+  },
+  methods: {
+    async loadImages() {
+      let listRef = this.storageRef.child(this.folder);
+      let firstPage = await listRef.list({ maxResults: 100 });
+      firstPage.items.forEach(item => {
+        this.references.push(item.location.path);
+        this.storageRef
+          .child(item.location.path)
+          .getDownloadURL()
+          .then(url => {
+            this.images.push(url);
+          })
+          .catch(error => {
+            console.log(error);
+          });
+      });
+    }
+  }
 };
 </script>
 
