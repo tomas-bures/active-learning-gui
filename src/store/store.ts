72f807019ba5bb2c43106c8e37fbb1ce4702d508
Tomáš Bureš <burgi22@seznam.cz>
2020-03-16 16:17:18 +0100

    Implement Image class

--- src/store/store.ts
+++ src/store/store.ts
@@ -2,11 +2,13 @@ import Dexie from "dexie";
 
 interface IAnnotation {
   id: number;
-  area: Array<number>;
+  area: number;
   category_id: number;
   image_id: number;
   iscrowd: number;
   segmentation: Array<number>;
+  bbox: Array<number>;
+  bbox_area: number;
 }
 interface ICategory {
   id: number;
@@ -15,13 +17,13 @@ interface ICategory {
 }
 interface IImage {
   id: number;
-  coco_url: string;
-  date_captured: string;
+  coco_url?: string;
+  date_captured?: string;
   file_name: string;
-  flickr_url: string;
+  flickr_url?: string;
   height: number;
   width: number;
-  licence: number;
+  licence?: number;
 }
 interface ILicenses {
   id: number;
@@ -32,13 +34,13 @@ interface ILicenses {
 export class DatasetDatabase extends Dexie {
   annotations: Dexie.Table<IAnnotation, number>;
   categories: Dexie.Table<ICategory, number>;
-  images: Dexie.Table<IImage, number>;
+  images: Dexie.Table<Image, number>;
   licenses: Dexie.Table<ILicenses, number>;
 
   constructor() {
     super("DatasetDatabase");
     this.version(1).stores({
-      annotations: "id, image_id, area, category_id",
+      annotations: "id, image_id, area, category_id, bbox_area",
       categories: "id, supercategory",
       images: "id, file_name",
       licenses: "id"
@@ -47,6 +49,58 @@ export class DatasetDatabase extends Dexie {
     this.categories = this.table("categories");
     this.images = this.table("images");
     this.licenses = this.table("licenses");
+    this.images.mapToClass(Image);
+    this.annotations.mapToClass(Annotation);
+  }
+}
+
+export class Image implements IImage {
+  id: number;
+  file_name: string;
+  height: number;
+  width: number;
+  annotations: Annotation[] | undefined;
+  constructor(id: number, file_name: string, height: number, width: number) {
+    this.id = id;
+    this.file_name = file_name;
+    this.height = height;
+    this.width = width;
+  }
+
+  async loadAnnotations() {
+    this.annotations = await database.annotations
+      .where("image_id")
+      .equals(this.id)
+      .toArray();
+  }
+}
+
+export class Annotation implements IAnnotation {
+  id: number;
+  area: number;
+  category_id: number;
+  image_id: number;
+  iscrowd: number;
+  segmentation: Array<number>;
+  bbox: Array<number>;
+  bbox_area: number;
+  constructor(
+    id: number,
+    area: number,
+    category_id: number,
+    image_id: number,
+    isCrowd: number,
+    segmentation: Array<number>,
+    bbox: Array<number>
+  ) {
+    this.id = id;
+    this.area = area;
+    this.category_id = category_id;
+    this.image_id = image_id;
+    this.iscrowd = isCrowd;
+    this.segmentation = segmentation;
+    this.bbox = bbox;
+    this.bbox_area = this.bbox.slice(-2).reduce((side, mul) => side * mul);
   }
 }
 
