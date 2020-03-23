72f58cd31f44ca4bfef7bc4c521d9594f920b174
Tomáš Bureš <burgi22@seznam.cz>
2020-03-23 13:22:32 +0100

    Create and implement drawPolygon function for polygon drawing

---
+++ src/paper/polygon.ts
@@ -0,0 +1,39 @@
+import paper from "paper";
+
+export interface ICoordinate {
+  x: number;
+  y: number;
+}
+
+export function drawPolygon(
+  vertices: Array<number>,
+  start: ICoordinate,
+  color: paper.Color
+) {
+  let polygon = new paper.Path();
+  polygon.fillColor = color;
+  polygon.strokeColor = color;
+  polygon.opacity = 0.5;
+  let second = false;
+  let x = 0;
+  let y = 0;
+  //For some reason cannot use for..of -> produces Uncaught exception: vertices[Symbol.iterator] is not a function
+  for (let i = 0; i < vertices.length; i++) {
+    let coordinate = vertices[i];
+    if (!second) {
+      x = start.x + coordinate;
+      second = true;
+    } else {
+      y = start.y + coordinate;
+      polygon.add(new paper.Point(x, y));
+      second = false;
+    }
+  }
+  polygon.onMouseEnter = function() {
+    this.selected = true;
+  };
+  polygon.onMouseLeave = function() {
+    this.selected = false;
+  };
+  return polygon;
+}
