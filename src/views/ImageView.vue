3d69d8ec6dd5160cb2b884696ace803988e62482
Tomáš Bureš <burgi22@seznam.cz>
2020-03-24 11:51:44 +0100

    Implement view for ImageModifier

---
+++ src/views/ImageView.vue
@@ -0,0 +1,13 @@
+<template>
+  <image-modifier />
+</template>
+
+<script>
+import ImageModifier from "../components/ImageModifier.vue";
+
+export default {
+  components: {
+    ImageModifier
+  }
+};
+</script>
