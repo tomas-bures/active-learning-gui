5ec6a27e7320c2d52aa3e6bff15508c9cc60ce4a
Tomáš Bureš <burgi22@seznam.cz>
2020-03-24 11:50:30 +0100

    Add ImageView to router

--- src/router/index.ts
+++ src/router/index.ts
@@ -3,6 +3,7 @@ import VueRouter from "vue-router";
 import Dashboard from "../views/Dashboard.vue";
 import Dataset from "../views/Dataset.vue";
 import Projects from "../views/Projects.vue";
+import ImageView from "../views/ImageView.vue";
 
 Vue.use(VueRouter);
 
@@ -21,6 +22,11 @@ const routes = [
     path: "/projects",
     name: "projects",
     component: Projects
+  },
+  {
+    path: "/image/:id",
+    name: "image",
+    component: ImageView
   }
 ];
 
