5a3aa91402be7462038fc007f1bf459b67d0bf0b
Tomáš Bureš <burgi22@seznam.cz>
2020-03-24 11:49:16 +0100

    Replace data properties with props

--- src/components/Picture.vue
+++ src/components/Picture.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="picture">
-    <v-card hover outlined :max-height="size + 10" :max-width="size + 10" class="test">
+    <v-card hover outlined :max-width="size + 10" class="test">
       <v-img
         class="grey darken-4 mx-1 my-1"
         :src="source"
@@ -8,6 +8,7 @@
         :height="size"
         :width="size"
       ></v-img>
+      <v-card-subtitle>Image ID: {{ id }}</v-card-subtitle>
     </v-card>
   </div>
 </template>
@@ -17,7 +18,8 @@ export default {
   data: () => ({}),
   props: {
     size: Number,
-    source: String
+    source: String,
+    id: Number
   }
 };
 </script>
