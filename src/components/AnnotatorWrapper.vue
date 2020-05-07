b710f5f24728cc39bb331688c32267f15638b722
Tomáš Bureš <burgi22@seznam.cz>
2020-05-08 00:07:31 +0200

    Make tooltips for buttons

--- src/components/AnnotatorWrapper.vue
+++ src/components/AnnotatorWrapper.vue
@@ -1,19 +1,52 @@
 <template>
   <div>
     <image-annotator :key="key" ref="test" />
-    <v-btn @click="loadPrevious" icon>
-      <v-icon>navigate_before</v-icon>
-    </v-btn>
-    <v-btn @click="rotateLeft" class="mx-2" icon>
-      <v-icon>rotate_left</v-icon>
-    </v-btn>
-    <v-btn @click="recenter" class="mx-2">Recenter</v-btn>
-    <v-btn @click="rotateRight" class="mx-2" icon>
-      <v-icon>rotate_right</v-icon>
-    </v-btn>
-    <v-btn @click="loadNext" icon>
-      <v-icon>navigate_next</v-icon>
-    </v-btn>
+    <v-tooltip top>
+      <template v-slot:activator="{ on }">
+        <v-btn @click="loadPrevious" icon v-on="on">
+          <v-icon>navigate_before</v-icon>
+        </v-btn>
+      </template>
+      <span>Load previous image</span>
+    </v-tooltip>
+    <v-tooltip top>
+      <template v-slot:activator="{ on }">
+        <v-btn @click="rotateLeft" class="mx-2" v-on="on" icon>
+          <v-icon>rotate_left</v-icon>
+        </v-btn>
+      </template>
+      <span>Rotate left</span>
+    </v-tooltip>
+    <v-tooltip top>
+      <template v-slot:activator="{ on }">
+        <v-btn @click="recenter" class="mx-2" v-on="on">Recenter</v-btn>
+      </template>
+      <span>Recenter</span>
+    </v-tooltip>
+    <v-tooltip top>
+      <template v-slot:activator="{ on }">
+        <v-btn @click="rotateRight" class="mx-2" v-on="on" icon>
+          <v-icon>rotate_right</v-icon>
+        </v-btn>
+      </template>
+      <span>Rotate right</span>
+    </v-tooltip>
+    <v-tooltip top>
+      <template v-slot:activator="{ on }">
+        <v-btn @click="loadNext" v-on="on" icon>
+          <v-icon>navigate_next</v-icon>
+        </v-btn>
+      </template>
+      <span>Load next</span>
+    </v-tooltip>
+    <v-tooltip top>
+      <template v-slot:activator="{ on }">
+        <v-btn @click="saveChanges" v-on="on" icon>
+          <v-icon>save</v-icon>
+        </v-btn>
+      </template>
+      <span>Save changes</span>
+    </v-tooltip>
   </div>
 </template>
 
@@ -49,6 +82,9 @@ export default {
     },
     loadNext() {
       this.load(true);
+    },
+    saveChanges() {
+      this.$refs.test.saveChanges();
     },
     async load(next) {
       const index = next
